b1c87f1f92698ea0288efdcbf67f22b046489c3f
Jack Stratton <jack@phroa.net>
2016-01-18 15:02:52 -0800

    build: print code version to SmartDashboard
    * build.xml
    
      This file is what's first looked at by the build system. Before
      continuing, it executes "git describe --tags". This command fetches
      the current commit number.
    
      It also makes a note of the current time, then proceeds to running the
      code in "custom_wpi_build.xml".
    
    * custom_wpi_build.xml
    
      This is a complete copy of the default build.xml file that ships with
      the WPILib plugin. It's normally located in the "wpilib" folder in
      your home folder.
    
      It has been modified slightly to add several attributes to the
      manifest file of the deployed code. Basically, it saves the
      (previously obtained) commit number, your username, and the current
      time to a special file inside the code deployment. These values can be
      retreived later, when the code is running. The values are listed here:
    
      | Value        | Meaning                                    | Example          |
      |--------------|--------------------------------------------|------------------|
      | Code-Version | The current git commit plus version number | 0.0.0-5-gab9ed68 |
      | Built-At     | Time and date when the code was deployed   | Jan 18, 03:08 PM |
      | Built-By     | Username of who deployed the code          | jack             |
    
    * OI
    
      Inside the constructor of the OI class, several lines have been added
      to fetch the aforementioned values from the manifest. The manifest is
      first loaded from the class loader, which knows where to find
      "resource" files inside the code deployment. The manifest is always
      named "MANIFEST.MF" inside the "META-INF" folder. Manifest objects
      have Attributes, which is a list of properties you can get by name.
    
      The operations are wrapped inside a try/catch block, because an error
      will pop up if you try to fetch a resource file that doesn't exist,
      even if it, well, always will exist. That's why there is very little
      error handling inside the catch block -- it'll probably never get
      called.

--- src/org/usfirst/frc/team4915/stronghold/OI.java
+++ src/org/usfirst/frc/team4915/stronghold/OI.java
@@ -5,35 +5,18 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.team4915.stronghold.commands.IntakeLauncher.IntakeBallCommandGroup;
 import org.usfirst.frc.team4915.stronghold.commands.IntakeLauncher.LaunchBallCommandGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 
 /**
- * This class is the glue that binds the controls on the physical operator
- * interface to the commands and command groups that allow control of the robot.
+ * This class handles the "operator interface", or the interactions between the
+ * driver station and the robot code.
  */
 public class OI {
-    //// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a
-    // joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
-
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
-
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
 
     // Start the command when the button is released and let it run the command
     // until it is finished as determined by it's isFinished method.
@@ -67,6 +50,23 @@ public class OI {
         // binds commands to buttons
         this.grabBallButton.whenPressed(new IntakeBallCommandGroup());
         this.launchBallButton.whenPressed(new LaunchBallCommandGroup());
+
+        try (InputStream manifest = getClass().getClassLoader().getResourceAsStream("META-INF/MANIFEST.MF")) {
+            Attributes attributes = new Manifest(manifest).getMainAttributes();
+
+            /* Print the attributes into form fields on the dashboard */
+            SmartDashboard.putString("Code Version", attributes.getValue("Code-Version"));
+            SmartDashboard.putString("Built At", attributes.getValue("Built-At"));
+            SmartDashboard.putString("Built By", attributes.getValue("Built-By"));
+
+            /* And print the attributes into the log. */
+            System.out.println("Code Version: " + attributes.getValue("Code-Version"));
+            System.out.println("Built At: " + attributes.getValue("Built-At"));
+            System.out.println("Built By: " + attributes.getValue("Built-By"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public Joystick getJoystickDrive() {
