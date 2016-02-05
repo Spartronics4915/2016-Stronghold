ad8ee28a182730352be1542606d7d04cda3d75c7
dbadb <dana.batali@gmail.com>
2016-02-05 23:47:15 +0000

    Roughed-in VisionState support to connect robot with jetson.
    A new husk singleton class, VisionState.java, is theorectically
    able to connect with both driver station and jetson. This is
    accomplished through the wonders of the SmartDashboard.
    Generally, robot requires read-only access to VisionState as
    the "owner" of the state is the jetson code. Currently
    our prototype vision app, imgExplorer.py, updates network
    tables via the python networktable implementation.
    
    Still to do (by Robin, Kevin and Julian):
    - verify  this  actually works before issuing a pull reqest
    - make connections to the smartdashboard GUI by:
      - implementing a smart dashboard type and widget
    - implement vision commands to respond to driver.

--- src/org/usfirst/frc/team4915/stronghold/vision/robot/VisionState.java
+++ src/org/usfirst/frc/team4915/stronghold/vision/robot/VisionState.java
@@ -1,40 +1,34 @@
 package org.usfirst.frc.team4915.stronghold.vision.robot;
-import java.util.Vector;
-import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.NamedSendable;
 import edu.wpi.first.wpilibj.tables.ITable;
 import edu.wpi.first.wpilibj.tables.ITableListener;
 
 /*
- * The VisionState class provides access within the Robot to the
- * values produced by the vision system. A subset of these values
- * can be made available to the SmartDashboard, but this separation
- * of interfaces may help to minimize the costs of change.
- *
+ * The VisionState class provides access within the Robot and the
+ * driver station to the values produced by the vision system. 
  */
 
-public class VisionState 
+public class VisionState implements NamedSendable
 {
-  private static final String s_tableName = "VisionState";
   private static VisionState s_instance;
 
   public synchronized static VisionState getInstance() 
   {
-    if (s_instance == null) {
+    if (s_instance == null) 
       s_instance = new VisionState();
-    }
     return s_instance;
   }
 
-  private NetworkTable m_visionTable;
-
-  public String driverRequest;
+  public String DriverRequest;
   public int FPS = 0;
-  public int targetsAcquired = 0;
-  public int targetX = 0;
-  public int targetY = 0;
-  public int targetSize = 0;
-  public float targetResponse = 0;
+  public int TargetsAcquired = 0;
+  public int TargetX = 0;
+  public int TargetY = 0;
+  public int TargetSize = 0;
+  public double TargetResponse = 0;
+  public int TargetClass = 0;
   
+  private ITable m_table=null;
   private final ITableListener m_listener = new ITableListener() 
   {
     @Override
@@ -45,39 +39,71 @@ public class VisionState
          * All numbers are stored as doubles in the network tables, event
          * those posted as int, float.
          */
-    	if(key ==  "driverRequest")
-    		s_instance.driverRequest = driverRequest;
+    	if(key ==  "DriverRequest")
+    		s_instance.DriverRequest = (String) value;
     	else
     	{
-	    	double  num = ((Double)value).doubleValue();
-	    	int ival = (int) ival;
+	    	double num = ((Double)value).doubleValue();
+	    	int ival = (int) num;
 	        if(key == "FPS")
 	            s_instance.FPS = ival;
 	        else
 	        if(key == "TargetsAcquired")
-	        	s_instance.targetsAcquired = ival;
+	        	s_instance.TargetsAcquired = ival;
 	        else
 	        if(key == "TargetX")
-	        	s_instance.targetX = ival;
+	        	s_instance.TargetX = ival;
 	        else
 	        if(key == "TargetY")
-	        	s_instance.targetY = ival;
+	        	s_instance.TargetY = ival;
 	        else
 	        if(key == "TargetSize")
-	        	s_instance.targetSize = ival;
+	        	s_instance.TargetSize = ival;
 	        else
 	        if(key == "TargetResponse")
-	        	s_instance.targetResponse = num;
+	        	s_instance.TargetResponse = num;
+	        else
+	        if(key == "TargetClass")
+	        	s_instance.TargetClass = ival;
     	}
     }
   };
 
-  /** VisionState constructor */
+  /** constructor is private, we're singleton for now */
   private VisionState() 
   {
-    m_visionTable = NetworkTable.getTable(s_tableName);
-    m_visionTable.addTableListenerEx(m_listener, 
-                            ITable.NOTIFY_NEW | ITable.NOTIFY_IMMEDIATE);
+  }
+  public String getName()
+  {
+	  return "Vision";
   }
 
+  public ITable getTable()
+  {
+    return m_table;
+  }
+
+  public String getSmartDashboardType()
+  {
+    return "Vision";
+  }
+
+  public void initTable(ITable subtable) 
+  {
+    if(this.m_table != null)
+        this.m_table.removeTableListener(m_listener);
+    this.m_table = subtable;
+    m_table.addTableListenerEx(m_listener,
+                            ITable.NOTIFY_NEW|ITable.NOTIFY_IMMEDIATE);
+
+    this.DriverRequest = m_table.getString("DriverRequest", "init");
+    this.FPS = (int) m_table.getNumber("FPS",  0.);
+    this.TargetsAcquired = (int) m_table.getNumber("TargetsAcquired", 0);
+    this.TargetX = (int) m_table.getNumber("TargetX", 0);
+    this.TargetY = (int) m_table.getNumber("TargetY", 0);
+    this.TargetSize = (int) m_table.getNumber("TargetSize", 0);
+    this.TargetResponse = m_table.getNumber("TargetResponse",  0.);
+    this.TargetClass = (int) m_table.getNumber("TargetClass", 0);
+    
+  }
 }
