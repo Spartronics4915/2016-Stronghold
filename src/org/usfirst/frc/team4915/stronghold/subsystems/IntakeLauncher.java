8c5e0dbb5576c0e98fd5c9df55b79a048ecfcdc3
Noah Martin <2013islandboy@gmail.com>
2016-02-22 15:55:01 -0800

    Smart Dashboard Fix
    Hi Noah again I messed up this should fix it thanks

--- src/org/usfirst/frc/team4915/stronghold/subsystems/IntakeLauncher.java
+++ src/org/usfirst/frc/team4915/stronghold/subsystems/IntakeLauncher.java
@@ -144,7 +144,7 @@ public class IntakeLauncher extends Subsystem {
     // Checks to see if joystick control or vision control is needed and
     // controls motion
     public void aimLauncher() {
-        SmartDashboard.putNumber("Launch Angle", degreesToTicks(getPosition()));
+        SmartDashboard.putNumber("Launch Angle", ticksToDegrees(getPosition()));
         if (VisionState.getInstance().wantsControl()) {
             trackVision();
         } else {
