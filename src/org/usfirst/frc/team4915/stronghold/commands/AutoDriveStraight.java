cb35dace72d0b3d9930828e4a5de73c8039a0385
Michaela Leung <mwl@sauria.com>
2016-03-12 21:41:10 -0800

    cleaned up the auto drive straight

--- src/org/usfirst/frc/team4915/stronghold/commands/AutoDriveStraight.java
+++ src/org/usfirst/frc/team4915/stronghold/commands/AutoDriveStraight.java
@@ -14,7 +14,7 @@ public class AutoDriveStraight extends Command {
 
     private double desiredDistanceTicks;
 
-    private boolean isInitialized, goBackward;
+    private boolean isInitialized;
 
     private int initializeRetryCount;
     private final static int MAX_RETRIES = 50;
@@ -22,12 +22,6 @@ public class AutoDriveStraight extends Command {
     public AutoDriveStraight(double desiredDistanceInches, double speed) {
         this.AUTOSPEED = speed;
         requires(Robot.driveTrain);
-        if (desiredDistanceInches < 0) {
-            goBackward = true;
-            desiredDistanceInches = -desiredDistanceInches;
-        }
-        else
-            goBackward = false;
         desiredDistanceTicks = inchesToTicks(desiredDistanceInches);
     }
 
@@ -48,11 +42,9 @@ public class AutoDriveStraight extends Command {
     		isInitialized = (RobotMap.leftMasterMotor.getEncPosition() == 0 &&
                              RobotMap.rightMasterMotor.getEncPosition() == 0);
     		initializeRetryCount++;
+    		
     	} else if (desiredDistanceTicks != 0) {
-            if (this.goBackward)
-                Robot.driveTrain.driveStraight(-AUTOSPEED);
-            else
-                Robot.driveTrain.driveStraight(AUTOSPEED);
+            Robot.driveTrain.driveStraight(AUTOSPEED); //speed can be positive or negative
         } else {
             SmartDashboard.putString("AutoDriveStraight: ", "No Ticks");
         }
@@ -89,11 +81,11 @@ public class AutoDriveStraight extends Command {
     	if(!isInitialized) {
     		if(initializeRetryCount >= MAX_RETRIES) {
     			SmartDashboard.putString("AutoDriveStraight: ", "INITIALIZE FAILED, MAXED OUT RETRIES");
+    			System.out.println("AutoDriveStraight: INITIALIZE FAILED, MAXED OUT RETRIES");
     			return true;
     		}
     		return false;
-    	} else if ((desiredDistanceTicks == 0) ||
-                (Math.abs(RobotMap.leftMasterMotor.getEncPosition()) >= desiredDistanceTicks) ||
+    	} else if ((Math.abs(RobotMap.leftMasterMotor.getEncPosition()) >= desiredDistanceTicks) ||
                 (Math.abs(RobotMap.rightMasterMotor.getEncPosition()) >= desiredDistanceTicks)) {
             return true;
         }
