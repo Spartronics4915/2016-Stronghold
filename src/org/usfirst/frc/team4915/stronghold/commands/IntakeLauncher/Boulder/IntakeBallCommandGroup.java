1bebfe7c9c07f7c79abb91b7057c09f1befa6a6b
Worldghoul <alex.j.cook@gmail.com>
2016-03-16 16:56:26 -0700

    Deleted unused commands

--- src/org/usfirst/frc/team4915/stronghold/commands/IntakeLauncher/Boulder/IntakeBallCommandGroup.java
+++ src/org/usfirst/frc/team4915/stronghold/commands/IntakeLauncher/Boulder/IntakeBallCommandGroup.java
@@ -2,7 +2,6 @@ package org.usfirst.frc.team4915.stronghold.commands.IntakeLauncher.Boulder;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.usfirst.frc.team4915.stronghold.commands.IntakeLauncher.Aimer.AimLauncherTravelForAutoCommand;
-import org.usfirst.frc.team4915.stronghold.commands.IntakeLauncher.Aimer.LauncherGoToTravelPositionCommand;
 
 public class IntakeBallCommandGroup extends CommandGroup {
     
