cb82211603db2ef2590d1d28f1834baf375a53b5
Michaela Leung <mwl@sauria.com>
2016-03-19 21:32:04 -0700

    pushing

--- src/org/usfirst/frc/team4915/stronghold/subsystems/IntakeLauncher.java
+++ src/org/usfirst/frc/team4915/stronghold/subsystems/IntakeLauncher.java
@@ -20,7 +20,7 @@ public class IntakeLauncher extends Subsystem {
     // indicate a wheel spinning inwards.
     private final double INTAKE_SPEED = -.60;
     private final double LAUNCH_SPEED_HIGH = 1.0;
-    private final double LAUNCH_SPEED_LOW = 0.6; // TODO
+    private final double LAUNCH_SPEED_LOW = 0.7; // TODO
     private final double ZERO_SPEED = 0.0;
     private final double AIM_DEGREES_SLOP = 2; // TODO: tune this number
 
@@ -33,8 +33,8 @@ public class IntakeLauncher extends Subsystem {
     private final double SERVO_LEFT_NEUTRAL_POSITION = .75; // in servo units
     private final double SERVO_RIGHT_NEUTRAL_POSITION = .4; // in servo units
 
-    private double launcherMaxHeightTicks = 832.0; 
-    private double launcherMinHeightTicks = 714.0; 
+    private double launcherMaxHeightTicks = 910.0; 
+    private double launcherMinHeightTicks = 739.0; 
     
     private double launcherTravelHeightRatio = 0.0;
     private double launcherNeutralHeightRatio = 1.0;
@@ -101,7 +101,7 @@ public class IntakeLauncher extends Subsystem {
 
     public void setDesiredWheelSpeed() {
          setSpeedLaunchLow();
-        }
+    }
  
 
     public String getDesiredWheelSpeed() {
@@ -234,7 +234,7 @@ public class IntakeLauncher extends Subsystem {
     // called periodically (even with the same setpoint) to prevent motorsafety
     // timeouts.
     public void moveToSetPoint() {
-        //keepSetPointInRange();
+        keepSetPointInRange();
         calibratePotentiometer();
         aimMotor.changeControlMode(TalonControlMode.Position); // redundant, but
                                                                // harmless
@@ -266,7 +266,7 @@ public class IntakeLauncher extends Subsystem {
     }
 
     public boolean canLaunch() {
-        return isLauncherAtTop();
+        return true;
     }
 
     // makes sure the set point doesn't go outside its max or min range
@@ -319,6 +319,7 @@ public class IntakeLauncher extends Subsystem {
     }
 
     public boolean isLauncherAtNeutral() {
+        System.out.println("Launcher is at Neutral:  " + (Math.abs(getPosition() - launcherNeutralHeightTicks()) < MAX_POTENTIOMETER_ERROR));
         return Math.abs(getPosition() - launcherNeutralHeightTicks()) < MAX_POTENTIOMETER_ERROR;
     }
 
